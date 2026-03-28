ac48dcb4566f53b0a4867a0a4b87ab7bb913b6da
miguel5612 <miguelangel5612@gmail.com>
2026-03-28 18:44:08 -0500

    feat(desktop): add configurable NovaWindows settings

--- src/FrameworkBase.Automation.Core/Configuration/DesktopSettings.cs
+++ src/FrameworkBase.Automation.Core/Configuration/DesktopSettings.cs
@@ -4,6 +4,16 @@ public sealed class DesktopSettings
 {
     public string DriverUri { get; init; } = "http://127.0.0.1:4723";
 
+    public string AutomationName { get; init; } = "Windows";
+
+    public string? WinAppDriverUrl { get; init; }
+
+    public int? SystemPort { get; init; }
+
+    public int CreateSessionTimeoutMilliseconds { get; init; } = 20000;
+
+    public int? WaitForAppLaunchSeconds { get; init; } = 5;
+
     public string ApplicationId { get; init; } = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
 
     public string DeviceName { get; init; } = "WindowsPC";
